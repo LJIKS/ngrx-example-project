897368928373ab5ff8d0a1348ecac7671279762c
LJIKS <L.Jansen@iks-gmbh.com>
2024-03-07 15:49:26 +0100

    basics implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ngrx-example-project';
+  public enable = 'Disabled';
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    MatButtonModule,
+  ],
+
+})
+export class AppModule {
+}
